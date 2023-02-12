0298a315ea6015efdecbde962af135586d4fec44
Michael Nau <michaelwnau@gmail.com>
2023-02-12 13:05:31 -0600

    fix dc yaml

--- workshop2/app/tutorials/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from tutorials.models import Tutorial
-
-
-class TutorialSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tutorial
-        fields = ('id', 'title', 'tutorial_url', 'image_path', 'description',
-                  'published')
